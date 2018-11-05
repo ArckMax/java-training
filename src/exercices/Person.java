641b49f44977edd3981c3430d6d4ed66a806f144
Maxime Gypteau <maxime.gypteau@gmail.com>
2018-11-05 09:35:41 +0100

    Réalisation quête Java-training 2

--- src/exercices/Person.java
+++ src/exercices/Person.java
@@ -6,21 +6,21 @@ public class Person {
     // - name (String)
     // - age (int)
     // - wilder (boolean)
-    String name;
-    int age;
-    boolean wilder;
+    private String name;
+    private int age;
+    private boolean wilder;
 
 
     // TODO 2. Add a Constructor with name and age arguments (in this order)
     // This constructor must initialize name and age attributes
-    Object Persona(String name, int age){
+    public Person(String name, int age){
         this.name = name;
         this.age = age;
     }
 
     // TODO 3. Add getter method for name attribute
 
-    public static String getName(){
+    public String getName(){
         return this.name;
     }
 
@@ -28,33 +28,31 @@ public class Person {
     // TODO 4. Add setter method for name attribute
 
 
-    public static Void setName(String param_name){
+    public void setName(String param_name){
         this.name = param_name;
     }
 
     // TODO 5. Add getter method for age attribute
 
-    public static int getAge(){
+    public int getAge(){
         return this.age;
     }
 
-
-
     // TODO 6. Add setter method for age attribute
 
-    public static Void setAge(int param_age){
+    public void setAge(int param_age){
         this.age = param_age;
     }
 
     // TODO 7. Add getter method for wilder attribute
 
-    public static boolean getWilder(){
+    public boolean isWilder(){
         return this.wilder;
     }
 
     // TODO 8. Add setter method for wilder attribute
 
-    public static void setWilder(boolean param_wilder){
+    public void setWilder(boolean param_wilder){
         this.wilder = param_wilder;
     }
 
@@ -62,7 +60,7 @@ public class Person {
     // TODO 9. Create a method 'whoAmI' that returns "My name is {name} and I'm {age}"
     // where you replace {name} and {age} with attributes values
 
-    public static String whoAmI(){
+    public String whoAmI(){
         return "My name is " + this.name + " and I'm " + this.age;
     }
 
